6cc7275b5deb551642fe8d02ed2bf326ad9f9af0
aziv7 <57321183+aziv7@users.noreply.github.com>
2023-08-19 16:11:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <main className='relative'>
+      <section className='xl:padding-1 wide:padding-r padding-b'></section>
+    </main>
+  );
 }
 
 export default App;
