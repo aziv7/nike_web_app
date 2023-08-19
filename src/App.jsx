0523c8b887146efbe0d5d2bef722d43dc0534307
aziv7 <57321183+aziv7@users.noreply.github.com>
2023-08-19 17:26:59 +0100

    importing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
+import Nav from '@/components/Nav.jsx';
+import PopularProducts from '@/components/PopularProducts';
+import SuperQuality from '@/components/SuperQuality';
+import Services from '@/components/Services';
+import SpecialOffer from '@/components/SpecialOffer';
+import CustomerReviews from '@/components/CustomerReviews';
+import Subscribe from '@/components/Subscribe';
+import Footer from '@/components/Footer';
+
 function App() {
   return (
     <main className='relative'>
-      <section className='xl:padding-1 wide:padding-r padding-b'></section>
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        <Nav />
+      </section>
 
-      <section className='padding'>PopularProducts</section>
-      <section className='padding'>SuperQuality</section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
       <section
         className='padding-x
 ру-10'>
-        Services
+        <Services />
       </section>
-      <section className='padding '>SpecialOffer</section>
-      <section className='padding bg-pale-blue'>CustomerReviews</section>
-      <section className='padding-x sm:py-32 py-16 w-full'>Subscribe</section>
-      <section className='padding-x padding-t pb-8 bg-black'>Footer</section>
+      <section className='padding '>
+        <SpecialOffer />
+      </section>
+      <section className='padding bg-pale-blue'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className='padding-x padding-t pb-8 bg-black'>
+        <Footer />
+      </section>
     </main>
   );
 }
