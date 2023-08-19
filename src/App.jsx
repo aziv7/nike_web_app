93322fe5d30ce1beabdccd04df4f28e5b087a0c2
aziv7 <57321183+aziv7@users.noreply.github.com>
2023-08-19 17:14:23 +0100

    nav links

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ function App() {
   return (
     <main className='relative'>
       <section className='xl:padding-1 wide:padding-r padding-b'></section>
+
+      <section className='padding'>PopularProducts</section>
+      <section className='padding'>SuperQuality</section>
+      <section
+        className='padding-x
+ру-10'>
+        Services
+      </section>
+      <section className='padding '>SpecialOffer</section>
+      <section className='padding bg-pale-blue'>CustomerReviews</section>
+      <section className='padding-x sm:py-32 py-16 w-full'>Subscribe</section>
+      <section className='padding-x padding-t pb-8 bg-black'>Footer</section>
     </main>
   );
 }
