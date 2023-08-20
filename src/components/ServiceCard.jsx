9f368b2f923ff17915e32e98c410920ec0780e7f
aziv7 <57321183+aziv7@users.noreply.github.com>
2023-08-20 21:04:36 +0100

    hover animation for service cards

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const ServiceCard = () => {
-  return <div>ServiceCard</div>;
+const ServiceCard = ({ subtext, imgURL, label }) => {
+  return (
+    <div
+      className='flex-1 
+  sm:w-[350px] sm:min-w-[350px]
+   w-full rounded-[20px] shadow-xl
+    px-10 py-16
+    transition-all hover:shadow-coral-red
+    hover:shadow-3xl
+    hover:-translate-y-3 duration-200
+    '>
+      <div
+        className='w-11 h-11 flex 
+  justify-center items-center bg-coral-red 
+  rounded-full'>
+        <img src={imgURL} alt={label} width={24} height={24} />
+      </div>
+      <h3
+        className='mt-5 font-palanquin text-3xl 
+  leading-normal font-bold'>
+        {label}
+      </h3>
+      <p
+        className='mt-3 break-words 
+  font-montserrat text-lg leading-normal 
+  text-slate-gray'>
+        {subtext}
+      </p>
+    </div>
+  );
 };
 
 export default ServiceCard;
